5a51bab79177ce207e4fc29579840883492e10bb
Aisa <aisajemila@gmail.com>
2019-04-12 12:57:43 +0300

    Deploy to  heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-export MOVIE_API_KEY='b3b7fe56df2349ea1307ca439dccd0fc'
-export SECRET_KEY='456789123'
-
+export MOVIE_API_KEY="b3b7fe56df2349ea1307ca439dccd0fc"
+export SECRET_KEY="456789123"
 python3.6 manage.py server
