6f063dd05caf0d0a8e56d3e1ccebd8a319f96b2c
Aisa <aisajemila@gmail.com>
2019-04-22 13:52:07 +0300

    Added databases and login for users.

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export MOVIE_API_KEY="b3b7fe56df2349ea1307ca439dccd0fc"
 export SECRET_KEY="456789123"
+export MAIL_USERNAME='aisajemila@gmail.com'
+export MAIL_PASSWORD='bella3bravo'
 python3.6 manage.py server
